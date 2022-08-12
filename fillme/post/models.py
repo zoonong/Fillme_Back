b767981277d09847ea2694c24f04bb7d0b8d98d4
dudtlstm <annpys1024@naver.com>
2022-08-13 04:05:26 +0900

    게시물 댓글 구현

--- fillme/post/models.py
+++ fillme/post/models.py
@@ -9,15 +9,19 @@ class Post(models.Model):
     persona = models.ForeignKey(Persona, on_delete = models.CASCADE)
     title = models.CharField(max_length = 200)
     content = models.TextField()
+    image1 = models.ImageField(upload_to="post/", blank = True, null = True)
+    image2 = models.ImageField(upload_to="post/", blank = True, null = True)
+    image3 = models.ImageField(upload_to="post/", blank = True, null = True)
+    image4 = models.ImageField(upload_to="post/", blank = True, null = True)
+    image5 = models.ImageField(upload_to="post/", blank = True, null = True)
+    image6 = models.ImageField(upload_to="post/", blank = True, null = True)
+    image7 = models.ImageField(upload_to="post/", blank = True, null = True)
+    image8 = models.ImageField(upload_to="post/", blank = True, null = True)
+    image9 = models.ImageField(upload_to="post/", blank = True, null = True)
+    image10 = models.ImageField(upload_to="post/", blank = True, null = True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
-    # 사진, 영상 필드 추가해야 함(사진은 image 모델 새로 생성해서 할 듯...!)
     # 좋아요 기능 추가해야 함
-
-class PostImage(models.Model):
-    id = models.AutoField(primary_key = True)
-    post = models.ForeignKey(Post, on_delete = models.CASCADE)
-    image = models.ImageField(upload_to="post/%Y/%m/%d", blank = True, null = True)
 
 class Comment(models.Model):
     id = models.AutoField(primary_key = True)
--- fillme/post/serializers.py
+++ fillme/post/serializers.py
@@ -1,33 +1,11 @@
 from rest_framework import serializers
 from .models import *
 
-# 사진
-class PostImageSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(use_url=True)
-
-    class Meta:
-        model = PostImage
-        fields = ['image']
-
 # 게시물
 class PostSerializer(serializers.ModelSerializer):
-    images = PostImageSerializer(many=True, read_only=True)
-    
-	#게시글에 등록된 이미지들 가지고 오기
-    def get_images(self, obj):
-        image = obj.image.all() 
-        return PostImageSerializer(instance=image, many=True, context=self.context).data
-
     class Meta:
         model = Post
-        fields = ['id', 'writer', 'persona', 'title', 'content', 'created_at', 'updated_at', 'images']          
-    
-    def create(self, validated_data):
-        instance = Post.objects.create(**validated_data)
-        image_set = self.context['request'].FILES
-        for image_data in image_set.getlist('image'):
-            PostImage.objects.create(post = instance, image = image_data)
-        return instance
+        fields = '__all__'       
 
 # 댓글
 class CommentSerializer(serializers.ModelSerializer):
