6cc90f82ebbc848ea22487334a5f76dde2d999f2
dudtlstm <annpys1024@naver.com>
2022-08-14 16:44:22 +0900

    좋아요 기능 구현

--- fillme/post/urls.py
+++ fillme/post/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('<int:post_pk>/', post_detail_update_delete, name = 'post_detail_update_delete'), # 특정 게시물 가져 오기 / 수정 / 삭제
     path('<int:post_id>/comments/', post_comment_list, name = 'post_comment_list'), # 특정 게시물의 댓글 보기 / 작성하기
     path('<int:post_pk>/comments/<int:comment_pk>/', post_comment_detail_update_delete, name = 'post_comment_detail_update_delete'), # 특정 게시물의 특정 댓글 보기 / 수정 / 삭제
+    path('<int:post_pk>/post_likes', post_likes, name = 'post_likes'), # 해당 게시물 좋아요 수 보기
+    path('<int:post_pk>/send_like', send_like, name = 'send_like') # 해당 게시물 속 좋아요 누르기 / 취소하기
 ]
