9b5001dc49b5c73c3e0cfe68b8f395902ee3c7e8
dudtlstm <annpys1024@naver.com>
2022-08-17 20:29:44 +0900

    내가 작성한 게시글 목록을 조회하는 api

--- fillme/post/urls.py
+++ fillme/post/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<int:post_pk>/', post_detail_update_delete, name = 'post_detail_update_delete'), # 사진 - 특정 게시물 가져 오기 / 수정 / 삭제
     path('video_post_create/', video_post_create, name = 'video_post_create'),# 영상 - 특정 게시물 작성하기
     path('<int:post_pk>/video_post_update_delete/', video_post_update_delete, name = 'video_post_update_delete'), # 영상 - 특정 게시물 가져 오기 / 수정 / 삭제
+    path('mypost/', my_post_list, name = 'my_post_list'), # 내가 작성한 게시글 목록을 조회하는 api
     path('<int:post_id>/comments/', post_comment_list, name = 'post_comment_list'), # 특정 게시물의 댓글 보기 / 작성하기
     path('<int:post_pk>/comments/<int:comment_pk>/', post_comment_detail_update_delete, name = 'post_comment_detail_update_delete'), # 특정 게시물의 특정 댓글 보기 / 수정 / 삭제
     path('<int:post_pk>/post_likes/', post_likes, name = 'post_likes'), # 해당 게시물 좋아요 수 보기
