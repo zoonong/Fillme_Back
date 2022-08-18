242576ed643c70d75a5bc635240ed580592a12ae
zoonong <zoonongzung@naver.com>
2022-08-19 02:03:42 +0900

    검색기록 삭제 url 오류 수정

--- fillme/usersearch/urls.py
+++ fillme/usersearch/urls.py
@@ -5,5 +5,5 @@ app_name="usersearch"
 urlpatterns = [
    path('',search_user,name='search_user'),
    path('history/',history_list_create,name='history_list_create'),
-   path('history/<int:history_id>',delete_history,name='delete_history'),
+   path('history/<int:history_id>/',delete_history,name='delete_history'),
 ]
